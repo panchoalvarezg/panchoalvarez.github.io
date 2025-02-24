89420e53457d44fa08871cad67e2c4655bba5081
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-02-24 12:34:32 -0300

    Update currency-converter.js

--- backend/currency-converter/currency-converter.js
+++ backend/currency-converter/currency-converter.js
@@ -4,10 +4,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/api/currencies', async (req, res) => {
-    // Fetch the list of currencies from an external API
     try {
-        const response = await axios.get('cur_live_ZfVEDFIn1OwGsREozlo3yavN0czLKoUt2kp5E9al');
-        const currencies = Object.keys(response.data.rates);
+        const response = await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_ZfVEDFIn1OwGsREozlo3yavN0czLKoUt2kp5E9al&currencies=EUR%2CUSD%2CCLP');
+        const currencies = Object.keys(response.data.data);
         res.json(currencies);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch currencies' });
@@ -17,8 +16,8 @@ app.get('/api/currencies', async (req, res) => {
 app.get('/api/convert', async (req, res) => {
     const { amount, from, to } = req.query;
     try {
-        const response = await axios.get(`https://api.currencyapi.com/v3/latest?apikey=cur_live_ZfVEDFIn1OwGsREozlo3yavN0czLKoUt2kp5E9al&currencies=EUR%2CUSD%2CCAD`);
-        const rate = response.data.rates[to];
+        const response = await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_ZfVEDFIn1OwGsREozlo3yavN0czLKoUt2kp5E9al&currencies=EUR%2CUSD%2CCLP');
+        const rate = response.data.data[to].value;
         const result = amount * rate;
         res.json({ result });
     } catch (error) {
